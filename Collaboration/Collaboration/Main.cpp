f6b2f3e2fec9fd760102d60350b1a18c609ac2c6
Conner Tenn <connertenn@gmail.com>
2015-08-26 22:05:14 -0400

    UP

--- Collaboration/Collaboration/Main.cpp
+++ Collaboration/Collaboration/Main.cpp
@@ -3,12 +3,26 @@
 
 /*Includes*/
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <Windows.h>
 using namespace std;
 
+#define DBOUT(s)								\
+{												\
+	std::wostringstream os_;					\
+	os_ << s;									\
+	OutputDebugStringW(os_.str().c_str());		\
+}
 
 void main()
 {
 	int a = 1;
 	a++;
+	DBOUT("\n" << "This Appears in the Output window" << "\n");
+	while (GetAsyncKeyState(VK_ESCAPE) >= 0)
+	{
+
+	}
 }
 
