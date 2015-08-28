bb1e1cd17d5015cd93c2f3d1e5bc91279bddb8ac
Conner Tenn <connertenn@gmail.com>
2015-08-27 22:46:22 -0400

    d

--- Collaboration/Collaboration/Main.cpp
+++ Collaboration/Collaboration/Main.cpp
@@ -1,25 +1,77 @@
-
+#define GLEW_STATIC
 //Program Entrypoint
 
 /*Includes*/
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <Windows.h>
+//#include <GL/glew.h>
+#include "Libraries\GLlibs\GL\glew.h"
+#include <GLFW/glfw3.h>
+#include "Utils.h"
+#include "ShaderProgram.h"
+#include "ShaderDat.h"
 using namespace std;
 
-#define DBOUT(s)								\
-{												\
-	std::wostringstream os_;					\
-	os_ << s;									\
-	OutputDebugStringW(os_.str().c_str());		\
-}
+vector<GLfloat> vertices = {
+   -1.0f, 1.0f, 0.0f,
+   -1.0f,-1.0f, 0.0f,
+	1.0f,-1.0f, 0.0f,
+
+	1.0f,-1.0f, 0.0f,
+	1.0f, 1.0f, 0.0f,
+   -1.0f, 1.0f, 0.0f
+};
+
+vector<GLfloat> texPos = {
+	
+
+	1.0f, 0.0f,
+	1.0f, 1.0f,
+	0.0f, 1.0f,
+
+	0.0f, 1.0f,
+	0.0f, 0.0f,
+	1.0f, 0.0f,
+};
+
 
 void main()
 {
-	DBOUT("\n" << "This Appears in the Output window" << "\n");
-	while (GetAsyncKeyState(VK_ESCAPE) >= 0)
-	{
+	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Game", nullptr, nullptr);
+
+	if (!window)
+	{
+		glfwTerminate();
 	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	VertexObject vao(vertices, texPos);
+	ShaderProgram program(basicVertex, basicFragment);
+	Texture tex("Tex/doge.png");
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+		vao.use();
+		program.use();
+		tex.use();
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == true)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 }
